cb030a408e253487347503c1045e5eaa52877045
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-25 11:59:45 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,6 +3,13 @@ class Dog
   attr_accessor :name, :breed, :age 
   @@all = []
   
-  
-  
-end
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
